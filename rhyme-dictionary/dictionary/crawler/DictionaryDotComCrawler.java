72e1eb15d193719ac3ff5cb683b5f955b0129214
rhironaga <rhironaga@58095746-d721-11de-8b8f-07bb6d2c2ff3>
2009-11-22 16:34:52 +0000

    RhymeCrawler updated to have a more robust regex and strip extra single-quote char.  Test written to avoid having a main method test (bad form) in the class.

--- rhyme-dictionary/dictionary/crawler/DictionaryDotComCrawler.java
+++ rhyme-dictionary/dictionary/crawler/DictionaryDotComCrawler.java
@@ -11,22 +11,18 @@ public class DictionaryDotComCrawler extends RhymeCrawler {
 	}
 
 	@Override
-	protected String processPageUrl(String word, String contents) {
-		Pattern prPat = Pattern.compile(".*Pronunciation: <tt>(.*?)</tt>.*", Pattern.DOTALL);
+	public String processPageUrl(String word, String contents) {
+		String proString;
+		Pattern prPat = Pattern.compile(".*Pronunciation: <tt>'(.*?)</tt>.*", Pattern.DOTALL);
 		Matcher prMat = prPat.matcher(contents);
 		if (prMat.matches()) {
-			System.out.println(word + "		" + prMat.group(1));
+			proString = prMat.group(1);
 		} else {
-//			System.out.println("faile");
+			System.err.println("no html regex match: " + word);
+			return null;
 		}
 		
-		return null;
-	}
-	
-	public static void main(String[] args) {
-		String test = "<br />Main Entry: <b>syph·i·lis</b><br /> Pronunciation: <tt>'sif-(&-)l&s</tt><br /> Function: <i>noun</i><br /> <b>:</b> a chronic contagious usually venereal and often congenital diseasethat is caused by a spirochete of the genus <i>Treponema</i> (<i>T. pallidum</i>) and if left untreated produces chancres, rashes, and systemic lesions in a clinical course with three stages continuedover many years called also <i>lues</i>; &#8212;see <a href=\"/medical/search?db=mwmed&nq=primarysyphilis\"><font size=\"-1\">PRIMARY SYPHILIS</font></a>, <ahref=\"/medical/search?db=mwmed&nq=secondarysyphilis\"><font size=\"-1\">SECONDARY SYPHILIS</font></a>, <a href=\"/medical/search?db=mwmed&nq=tertiarysyphilis\"><font size=\"-1\">TERTIARY SYPHILIS</font></a><br /><b>Sypháiálus</b> <tt>/'sif-&-l&s,/</tt> literary character. Syphilus is the hero of the 1530 Latin poem “Syphilis or the French Disease,” which was written by the Italianphysician and poet Girolamo Fracastoro. Syphilus is a swineherd who blasphemes a sun god and as a punishment is afflicted with a disease.\nhiethere";
-		DictionaryDotComCrawler t = new DictionaryDotComCrawler();
-		t.processPageUrl("fea", test);
+		return proString;
 	}
 
 }
