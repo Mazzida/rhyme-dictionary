7742a3426a664ca9f0597e24dfe8cf4aa7ce1a7b
rhironaga <rhironaga@58095746-d721-11de-8b8f-07bb6d2c2ff3>
2009-11-23 03:32:19 +0000

    Much improved regular expression for leeching pronunciation text from the html text.  Returns the special-char version, not just the pronunciation text.

--- rhyme-dictionary/dictionary/crawler/DictionaryDotComCrawler.java
+++ rhyme-dictionary/dictionary/crawler/DictionaryDotComCrawler.java
@@ -14,7 +14,8 @@ public class DictionaryDotComCrawler extends RhymeCrawler {
 	public String processPageUrl(String word, String contents) {
 		String proString;
 //		Pattern prPat = Pattern.compile(".*Pronunciation: <tt>'(.*?)</tt>.*", Pattern.DOTALL);
-		Pattern prPat = Pattern.compile(".*<span class=\"pron\">(.*?)</span><span class=\"prondelim\">].*", Pattern.DOTALL);
+//		Pattern prPat = Pattern.compile(".*<span class=\"pron\">(.*?)</span><span class=\"prondelim\">].*", Pattern.DOTALL);
+		Pattern prPat = Pattern.compile(".*&nbsp;&nbsp;(.*?)&nbsp;<a title=\"Click for guide to symbols.\".*", Pattern.DOTALL);
 		Matcher prMat = prPat.matcher(contents);
 		if (prMat.matches()) {
 			proString = prMat.group(1);
