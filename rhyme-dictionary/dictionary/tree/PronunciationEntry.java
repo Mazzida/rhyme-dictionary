ba753226785244b0b3e9ac574b4ebb8c30008116
rhironaga <rhironaga@58095746-d721-11de-8b8f-07bb6d2c2ff3>
2009-11-29 21:09:33 +0000

    In progress: parsing the pronunciation dictionary into a data structure element to have primary/secondary pronunciations available in an easily manageable format for insertion into the db

--- rhyme-dictionary/dictionary/tree/PronunciationEntry.java
+++ rhyme-dictionary/dictionary/tree/PronunciationEntry.java
@@ -1,14 +1,46 @@
 package dictionary.tree;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class PronunciationEntry {
 
+	private String word;
+	private ArrayList<Integer[]> syllables;
+	private ArrayList<Integer[]> alternates;
+
 	public PronunciationEntry() {
-		
+		word = "";
+		syllables = new ArrayList<Integer[]>();
+		alternates = new ArrayList<Integer[]>();
 	}
 	
 	public static PronunciationEntry makeEntry(String aEntryString) {
-		PronunciationEntry output = null;
-		//TODO
+		PronunciationEntry output = new PronunciationEntry();
+		aEntryString = aEntryString.trim();
+		Pattern prPat = Pattern.compile("(.*?)\t\\((.*)\\)", Pattern.DOTALL);
+		Matcher prMat = prPat.matcher(aEntryString);
+
+		if (prMat.matches()) {
+			output.word = prMat.group(1);
+			String prString = prMat.group(2);
+			String[] list = prString.split(", ");
+			String[] primary = list[0].split("['-]");
+
+			for (int altInd = 1; altInd < list.length; altInd ++) {
+				System.out.println("process: " + list[altInd]);
+				String[] secondary = list[altInd].split("['-]");
+				System.out.println("\t\t\t" + Arrays.toString(secondary));
+			}
+			System.out.println("\t\t\t" + Arrays.toString(list) + "\t\t" + list.length);
+			System.out.println("\t\t\t" + Arrays.toString(primary));
+			System.out.println("\n\n");
+		} else {
+			throw new IllegalArgumentException("Failed Parse: ");
+		}
+
 		return output;
 	}
 }
