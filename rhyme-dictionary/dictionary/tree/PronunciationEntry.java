198502d53d75f83245c73047b348bcf942245e7e
rhironaga <rhironaga@58095746-d721-11de-8b8f-07bb6d2c2ff3>
2009-11-29 21:34:32 +0000

    Primary pronunciation collection complete, need to implement addition of secondary pronunciations, which may be tricky with edit distances over potential positions within parent string.

--- rhyme-dictionary/dictionary/tree/PronunciationEntry.java
+++ rhyme-dictionary/dictionary/tree/PronunciationEntry.java
@@ -8,39 +8,56 @@ import java.util.regex.Pattern;
 public class PronunciationEntry {
 
 	private String word;
-	private ArrayList<Integer[]> syllables;
-	private ArrayList<Integer[]> alternates;
+	private ArrayList<Integer> syllables;
 
 	public PronunciationEntry() {
 		word = "";
-		syllables = new ArrayList<Integer[]>();
-		alternates = new ArrayList<Integer[]>();
+		syllables = new ArrayList<Integer>();
 	}
 	
-	public static PronunciationEntry makeEntry(String aEntryString) {
-		PronunciationEntry output = new PronunciationEntry();
+	public static ArrayList<PronunciationEntry> makeEntry(String aEntryString) {
+		ArrayList<PronunciationEntry> output = new ArrayList<PronunciationEntry>();
 		aEntryString = aEntryString.trim();
+		PronunciationEntry tEntry;
+		String word;
 		Pattern prPat = Pattern.compile("(.*?)\t\\((.*)\\)", Pattern.DOTALL);
 		Matcher prMat = prPat.matcher(aEntryString);
 
 		if (prMat.matches()) {
-			output.word = prMat.group(1);
+			word = prMat.group(1);
 			String prString = prMat.group(2);
 			String[] list = prString.split(", ");
 			String[] primary = list[0].split("['-]");
 
+			tEntry = new PronunciationEntry();
+			tEntry.word = word;
+			tEntry.syllables = getSyllableKeys(primary);
+			output.add(tEntry);
+
 			for (int altInd = 1; altInd < list.length; altInd ++) {
-				System.out.println("process: " + list[altInd]);
+				
 				String[] secondary = list[altInd].split("['-]");
-				System.out.println("\t\t\t" + Arrays.toString(secondary));
 			}
-			System.out.println("\t\t\t" + Arrays.toString(list) + "\t\t" + list.length);
-			System.out.println("\t\t\t" + Arrays.toString(primary));
-			System.out.println("\n\n");
 		} else {
 			throw new IllegalArgumentException("Failed Parse: ");
 		}
 
 		return output;
+	}
+
+	private static ArrayList<Integer> getSyllableKeys(String[] syllables) {
+		ArrayList<Integer> output = new ArrayList<Integer>();
+		for (String syllable : syllables) {
+			output.add(SyllableHash.insert(syllable));
+		}
+		return output;
+	}
+	
+	public String toString() {
+		String syllableVals = "";
+		for (Integer syllable : syllables) {
+			syllableVals += SyllableHash.get(syllable) + "-";
+		}
+		return String.format("[ %s %s %s ]", word, syllables.toString(), syllableVals);
 	}
 }
