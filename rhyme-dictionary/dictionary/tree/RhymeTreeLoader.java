3439244332c85de5aa6fe2b64e26facfe9a94565
rhironaga <rhironaga@58095746-d721-11de-8b8f-07bb6d2c2ff3>
2009-11-29 07:13:43 +0000

    Added support for a 2-way hash table between integer keys, String syllable values for space-efficient storage of the tree (to hold over 100k entries).  This takes advantage of duplicate syllable values

---
+++ rhyme-dictionary/dictionary/tree/RhymeTreeLoader.java
@@ -0,0 +1,25 @@
+package dictionary.tree;
+
+import java.io.File;
+import java.util.Scanner;
+
+public class RhymeTreeLoader {
+
+	private static final String PRONUNCIATION_DICTIONARY = "pronunciations.txt";
+
+	public RhymeTree loadTree() {
+		RhymeTree tree = new RhymeTree();
+		try {
+			Scanner fReader = new Scanner( new File(PRONUNCIATION_DICTIONARY));
+			while (fReader.hasNextLine()) {
+				String entryString = fReader.nextLine();
+				
+			}
+		} catch(Exception e) {
+			System.err.println("ERROR: tree load failure");
+			System.err.println(e.getLocalizedMessage());
+		}
+		return tree;
+	}
+
+}
