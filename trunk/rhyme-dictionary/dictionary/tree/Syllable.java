467e3c309d781537266e0e9765d97e6d99e1b6e7
rhironaga <rhironaga@58095746-d721-11de-8b8f-07bb6d2c2ff3>
2009-12-09 15:41:39 +0000

    Added matcher strategy to loading technique, to include stressed vs unstressed characteristics of syllables.  Need to test for corner cases.

--- trunk/rhyme-dictionary/dictionary/tree/Syllable.java
+++ trunk/rhyme-dictionary/dictionary/tree/Syllable.java
@@ -3,9 +3,11 @@ package dictionary.tree;
 public class Syllable {
 
 	private String syllable;
+	private boolean stressed;
 
-	protected Syllable(String aSyllable) {
+	protected Syllable(String aSyllable, boolean aStressed) {
 		syllable = aSyllable;
+		stressed = aStressed;
 	}
 
 	public int hashCode() {
@@ -15,11 +17,11 @@ public class Syllable {
 	public boolean equals(Object o) {
 		if (o instanceof Syllable) {
 			Syllable s = (Syllable) o;
-			return s.syllable.equals(syllable);
+			return s.syllable.equals(syllable) && s.stressed == stressed;
 		}
 		return false;
 	}
 
-	public static final Syllable NULL = new Syllable("");
+	public static final Syllable NULL = new Syllable("", false);
 
 }
