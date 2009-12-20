945b35ecf851b2b0e7c89b24edcd92741cdfa275
rhironaga <rhironaga@58095746-d721-11de-8b8f-07bb6d2c2ff3>
2009-12-20 03:47:40 +0000

    Initial setup for a Main class that initializes a CLI for user input through a command line or to be called through another program.  User input is parsed and processed in a simple loop.
    Still need to work on discrimination when there are words with many rhyming final stressed vowels.

---
+++ trunk/rhyme-dictionary/dictionary/main/Main.java
@@ -0,0 +1,14 @@
+package dictionary.main;
+
+public class Main {
+
+	public static void main(String[] args) {
+		if (args.length == 0) {
+			RhymeDictionaryCLI.start();
+		} else {
+			for (String arg : args) {
+				RhymeDictionaryCLI.process(arg);
+			}
+		}
+	}
+}
--- trunk/rhyme-dictionary/dictionary/main/RhymeQueryHandler.java
+++ trunk/rhyme-dictionary/dictionary/main/RhymeQueryHandler.java
@@ -18,10 +18,20 @@ public class RhymeQueryHandler {
 		trie = RhymeTreeLoader.loadTree();
 	}
 
+	/**
+	 * Returns the set of rhyming words, or null if the
+	 * argument word was not found in the dataset
+	 */
 	public static Set<String> getRhymes(String aWord) {
 		return singleton.trie.getRhymes(aWord);
 	}
 
+	/**
+	 * Returns the set of strict rhyming words, or null if the
+	 * argument word was not found in the dataset.  A strict rhyme 
+	 * occurs when words match in both final stressed vowel sounds
+	 * and all subsequent syllables.
+	 */
 	public static Set<String> getStrictRhymes(String aWord) {
 		return singleton.trie.getStrictRhymes(aWord);
 	}
